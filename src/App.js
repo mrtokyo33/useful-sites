047500d8931112bc1f842ab9c5d12d8d1db9b28a
mrtokyo <mrtokyo33sec@gmail.com>
2025-01-20 15:22:23 -0300

    changing into react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+
+      <main>
+
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './css/Header.module.css';
+
+function Header() {
+  return (
+    <header>
+        <div className={styles.logo}>
+            <h1>Some Useful Links!</h1>
+        </div>
+      
+        <nav>
+            <ul className={styles.navUl}>
+                <li className={styles.navItem}><a href="index.html" target="_self" rel="nofollow">Home</a></li>
+                <li className={styles.navItem}><a href="https://github.com/mrtokyo33/useful-sites" target="_blank" rel="noopener noreferrer">Repository</a></li>
+            </ul>
+        </nav>
+
+        <div className={styles.searchAndLogin}>
+            <div className={styles.searchContainer}>
+                <i className="fa fa-search"></i>
+                <input type="text" name="search" placeholder="Search..." />
+            </div>
+            <div className={styles.loginIconContainer}>
+                <a href="login.html">
+                <i className="fas fa-user"></i>
+                </a>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
